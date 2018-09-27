4d736d41ddfdb757396cf14f918d26fcd7a75ef1
sv28009330 <43635965+sv28009330@users.noreply.github.com>
2018-09-27 14:18:45 +0530

    Update shivam.cpp
    Addition of three no.

--- shivam.cpp
+++ shivam.cpp
@@ -2,10 +2,14 @@
 #include<conio.h>
 void main()
 {
-int a,b,c;
+int a,b,c,d;
 cout<<"Enter the no.";
 cin>>a>>b>>c;
 a=b+c;
 cout<<"Addition of three numbers is="<<a;
+d=a+b+c;
+cout<<"Enter fourth value";
+cin>>d;
+cout<<"addition of three numbers is="<<d;
 getch();
 }
